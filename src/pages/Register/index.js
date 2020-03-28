90355f49e0869427cfc61df1deb948cd2cc1915b
lucas sarri <lucassarri@hotmail.com>
2020-03-27 23:03:33 -0300

    alteracoes dia 27

---
+++ src/pages/Register/index.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+import {FiArrowLeft} from 'react-icons/fi';
+import api from '../../services/api';
+import './styles.css';
+import logoImg from '../../assets/logo.svg';
+
+export default function Login() {
+    //Setando os estados (valores dos inputs)
+    const [name,SetName] = useState('');
+    const [email,SetEmail] = useState('');
+    const [whatsApp,SetWhatsApp] = useState('');
+    const [city,SetCity] = useState('');
+    const [uf,SetUf] = useState('');
+
+    function hadleRegister(e)
+    {
+        e.preventDefault();
+        console.log(name,email,whatsApp,city,uf);
+    }
+
+    return(
+       <div className="register-container">
+           <div className="content">
+               <section>
+                    <img src={logoImg} alt="Be The Hero"/>
+                    <h1>Cadastro</h1>
+                    <p>Faça seu cadastro, entre na plataforma para ajudar pessoas a encontrar animais perdidos ou para doação de animais</p>
+                    <Link className='back-link' to="/">
+                        <FiArrowLeft size={16} color='#E02041'/>
+                        Voltar para Login
+                    </Link>
+               </section>
+               <form onSubmit={hadleRegister}>
+                    <input value={name} onChange={e=>SetName(e.target.value)} placeholder='Nome do Usuário'/>
+                    <input value={email}  onChange={e=>SetEmail(e.target.value)} type="email" placeholder='Email'/>
+                    <input value={whatsApp}  onChange={e=>SetWhatsApp(e.target.value)} placeholder='WhatsApp' />
+                    <div className="input-group">
+                        <input value={city}  onChange={e=>SetCity(e.target.value)} placeholder='Cidade'/>
+                        <input value={uf}  onChange={e=>SetUf(e.target.value)} placeholder='UF' style={ {width:80} }/>
+                    </div>
+                    <button className="button">
+                        Cadastrar
+                    </button>
+               </form>
+           </div>
+       </div>
+    );
+}
